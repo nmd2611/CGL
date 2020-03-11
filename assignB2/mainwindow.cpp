2171cb1c60b391ef4b3ce45640c63d63d402a69c
Nimish Dadlani <nmd2611@gmail.com>
2020-03-11 15:47:02 +0530

    assign b2

---
+++ assignB2/mainwindow.cpp
@@ -0,0 +1,104 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QLabel>
+#include "QMouseEvent"
+#include "QColorDialog"
+#include <cmath>
+#include<bits/stdc++.h>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+QImage image(700,700,QImage::Format_RGB888);
+QRgb value = qRgb(0,255,0);
+QColor value2;
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+value2=QColorDialog::getColor();
+ui->label->show();
+
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *ev)
+{
+if(ev->button() == Qt::LeftButton && flag == 0)
+{
+     x[i]=ev->pos().x()-200 ;
+     y[i]=ev->pos().y() +130;
+
+     if(i != 0)
+        DDALine(x[i-1],y[i-1],x[i],y[i]);
+   // image.setPixel(x[i],y[i],value);
+    i++;
+}
+
+if(ev->button() == Qt::RightButton && flag == 0)
+{
+    flag=1;
+    DDALine(x[i-1],y[i-1],x[0],y[0]);
+
+}
+
+else if(ev->button() == Qt::RightButton && flag == 1)
+   {
+    flag=2;
+    int m=ev->pos().x() - 200;
+    int n=ev->pos().y() +130;
+
+    QRgb q= image.pixel(m,n);
+    FloodFill(m,n,q);
+    ui->label->setPixmap(QPixmap::fromImage(image));
+   }
+
+}
+
+void MainWindow::DDALine(int x1,int y1, int x2, int y2)
+{
+    float x,y;
+    float dx,dy;
+
+    dx=x2-x1;     // Calculating dx
+    dy=y2-y1;     // Calculating dy
+
+    int steps=(abs(dx)>abs(dy))?abs(dx):abs(dy);
+
+    float xinc,yinc;
+
+    if(steps != 0)          // Calculating Xinc and Yinc
+    {
+         xinc= dx/ steps;
+         yinc= dy/ steps;
+    }
+    x=x1+0.5;
+    y=y1+0.5;
+    while(steps--)
+    {
+        image.setPixel(x,y,value2.rgb());  //draws pixel with value
+        //print pixel
+        x+= xinc;
+        y+= yinc;
+    }
+    ui->label->setPixmap(QPixmap::fromImage(image));
+}
+
+void MainWindow::FloodFill(int x, int y,QRgb q)
+{
+if(image.pixel(x,y) == q)
+{
+    image.setPixel(x,y,value);
+    FloodFill(x,y+1,q);
+    FloodFill(x,y-1,q);
+    FloodFill(x+1,y,q);
+    FloodFill(x-1,y,q);
+}
+//ui->label->setPixmap(QPixmap::fromImage(image));
+}
---
+++ assignB2/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+    void mousePressEvent(QMouseEvent *ev);
+    void DDALine(int x1,int y1,int x2,int y2);
+    void FloodFill(int x, int y,QRgb q);
+
+
+private:
+    Ui::MainWindow *ui;
+    int x[100];
+    int y[100];
+    int flag=0;
+    int i=0;
+
+};
+
+#endif // MAINWINDOW_H
